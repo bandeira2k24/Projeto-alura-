6cd5f57d1bd94da67af29b094e5af2c2603539ac
bandeira2k24 <gustavobandeira82@gmail.com>
2025-08-29 16:13:12 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Script simples para mensagem de envio do formulário
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Mensagem enviada com sucesso! Obrigado por entrar em contato.");
+});
